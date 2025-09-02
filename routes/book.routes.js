92e0f04cc08b331bdceb02e863fb7b1c43276499
Gabor Farkas <farkas.gz@gmail.com>
2025-09-02 15:32:50 +0200

    added books-list route and page

---
+++ routes/book.routes.js
@@ -0,0 +1,24 @@
+// routes/book.routes.js
+
+const router = require("express").Router();
+
+const Book = require("../models/Book.model.js"); // <== add this line before your routes
+
+// GET route to retrieve and display all the books
+router.get("/books", (req, res, next) => {
+  Book.find()
+    .then((allTheBooksFromDB) => {
+      // -> allTheBooksFromDB is a placeholder, it can be any word
+      console.log("Retrieved books from DB:", allTheBooksFromDB);
+
+      res.render("books/books-list.ejs", { books: allTheBooksFromDB });
+    })
+    .catch((error) => {
+      console.log("Error while getting the books from the DB: ", error);
+
+      // Call the error-middleware to display the error page to the user
+      next(error);
+    });
+});
+
+module.exports = router;
